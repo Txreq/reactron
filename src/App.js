fc533e24e3dee427644938a3f5899887c94eb01e
Txreq™ <58333332+Txreq@users.noreply.github.com>
2021-07-04 15:04:23 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./style.css";
+const os = require("os");
 
-const os = require("os")
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      cpu: {
+        model: "",
+        cores: 0,
+        speed: 0,
+      },
+      mem: {
+        free: 0,
+        total: 0
+      }
+    }
+  }
 
-function App() {
-  return (
-    <div>
-      <h1>Hello {os.userInfo().username}</h1>
-    </div>
-  );
+  componentDidMount(){
+    this.setState({
+      cpu: {
+        model:  os.cpus()[0].model,
+        cores:  os.cpus().length,
+        speed: os.cpus()[0].speed
+      }
+    })
+
+    setInterval(() => {
+      this.setState({
+        mem: {
+          free: os.freemem(),
+          total: os.totalmem(),
+        }
+      })
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello {os.userInfo().username} 👋</h1>
+
+        <div className="infos">
+          <p>{this.state.cpu.model} ({this.state.cpu.cores} cores) {this.state.cpu.speed}Mhz</p>
+          <p>{Math.floor(this.state.mem.total / 1024 / 1024)}Mbps ({Math.floor(this.state.mem.free / 1024 / 1024)}Mbps free)</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,5 +6,4 @@ let root = document.createElement('div');
 root.id = "root";
 document.body.appendChild( root );
 
-// Now we can render our application into it
-render( <App />, document.getElementById('root') );
+render(<App />, document.getElementById('root'));
